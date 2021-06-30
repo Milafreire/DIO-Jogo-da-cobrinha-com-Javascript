c8477325c53b6967a7343f7f8fe7f39c436ee90c
Mila Freire <camilabsfreire@gmail.com>
2021-06-30 19:45:22 -0300

    Alt#3

--- script.js
+++ script.js
@@ -7,17 +7,26 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 +1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "pink";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "lightgreen";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -29,15 +38,22 @@ function update (event){
     if(event.keyCode == 40 && direction != "up") direction = "down";
 }
 
-function iniciarJogo(){
+function iniciarJogo(){    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; 1 < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over! :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX= snake[0].x;
     let snakeY = snake[0].y;
@@ -46,8 +62,14 @@ function iniciarJogo(){
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
-    snake.pop();
 
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+ 
     let newHead = {
         x: snakeX,
         y: snakeY
